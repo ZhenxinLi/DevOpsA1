dc55dcec230fd446c00621d632b6471bb52db5e9
Zhenxin Li <59156022+ZhenxinLi@users.noreply.github.com>
2022-04-02 15:10:54 +1100

    removed unused var3 10:7

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -7,10 +7,6 @@ router.get('/new', (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  let note = await new Note({
-    title: req.body.title,
-    description: req.body.description,
-  });
   try {
     note = await note.save();
     res.redirect('/');
